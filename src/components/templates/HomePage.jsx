a87fd741a910a220578c2dcf43a8fef3ec7960d0
Afshin Zarei <afshin.zarei2000@gmail.com>
2024-04-29 19:38:33 +0330

    Added pagination section

--- src/components/templates/HomePage.jsx
+++ src/components/templates/HomePage.jsx
@@ -1,20 +1,28 @@
 import { useEffect, useState } from "react";
 import TableCoin from "../modules/TableCoin";
 import { getCoinsList } from "../../services/cryptoAPI";
+import Pagination from "../modules/Pagination";
 
 function HomePage() {
   const [coins, setCoins] = useState([]);
-  const [isLoading, setIsLaoding] = useState(true) 
+  const [isLoading, setIsLaoding] = useState(true);
+  const [page, setPage] = useState(1);
   useEffect(() => {
+    setIsLaoding(true);
     const getData = async () => {
-      const res = await fetch(getCoinsList());
+      const res = await fetch(getCoinsList(page));
       const json = await res.json();
       setCoins(json);
-      setIsLaoding(false)
+      setIsLaoding(false);
     };
     getData();
-  }, []);
-  return <TableCoin coins={coins} isLoading={isLoading} />;
+  }, [page]);
+  return (
+    <>
+      <TableCoin coins={coins} isLoading={isLoading} />
+      <Pagination page={page} setPage={setPage} />
+    </>
+  );
 }
 
 export default HomePage;
